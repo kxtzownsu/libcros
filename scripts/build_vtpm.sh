4a31ec10c4db2d01bf44f002da2f185fe80ed1a4
kxtzownsu <kxtzownsu@gmail.com>
2026-04-02 22:40:28 -0400

    bug: fix typo in Makefile
    Signed-off-by: kxtzownsu <kxtzownsu@gmail.com>

--- scripts/build_vtpm.sh
+++ scripts/build_vtpm.sh
@@ -19,7 +19,7 @@ cat <<EOF > "${BUILD_DIR}/Makefile"
 obj-m += tpm_vtpm_proxy.o
 
 all:
-	make -C /lib/modules/\$(shell uname -r/build M=${BUILD_DIR} modules
+	make -C /lib/modules/\$(shell uname -r)/build M=${BUILD_DIR} modules
 
 clean:
 	make -C /lib/modules/\$(shell uname -r)/build M=${BUILD_DIR} clean
