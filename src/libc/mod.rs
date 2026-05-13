32c7a312c4d5328418ee706efc14e86ab32dae95
kxtzownsu <kxtzownsu@gmail.com>
2026-05-13 10:35:24 -0400

    feat: add libc::errno
    Signed-off-by: kxtzownsu <kxtzownsu@gmail.com>

--- src/libc/mod.rs
+++ src/libc/mod.rs
@@ -7,7 +7,7 @@ pub mod mkdir;
 pub mod mount;
 pub mod umount;
 // pub mod tmpfs;
-// pub mod errno;
+pub mod errno;
 
 pub use ftruncate::*;
 pub use ioctl::*;
@@ -15,4 +15,4 @@ pub use mkdir::*;
 pub use mount::*;
 pub use umount::*;
 // pub use tmpfs::*;
-// pub use errno::*;
+pub use errno::*;
