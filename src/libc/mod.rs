e35be138044d3bf3dcf6bf1fe44523306cfad226
kxtzownsu <kxtzownsu@gmail.com>
2026-05-13 10:24:28 -0400

    feat: enable libc::{ftruncate, ioctl, umount}
    Signed-off-by: kxtzownsu <kxtzownsu@gmail.com>

--- src/libc/mod.rs
+++ src/libc/mod.rs
@@ -1,8 +1,18 @@
 pub const AT_FDCWD: isize = -100;
 
 pub mod asm;
-pub mod mount;
+pub mod ftruncate;
+pub mod ioctl;
 pub mod mkdir;
+pub mod mount;
+pub mod umount;
+// pub mod tmpfs;
+// pub mod errno;
 
-pub use mount::*;
+pub use ftruncate::*;
+pub use ioctl::*;
 pub use mkdir::*;
+pub use mount::*;
+pub use umount::*;
+// pub use tmpfs::*;
+// pub use errno::*;
